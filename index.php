b9b804bddbd5a0e7708ebbdcc1a6d9699654e9b0
Valentino <auikwok2@gmail.com>
2022-01-23 13:50:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
     <title>Coba git remote</title>
 </head>
 <body>
-    <h1>Hello World!
+    <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum modi molestiae, alias dolorem accusantium saepe consequatur dolorum amet at natus earum eos numquam? Magnam molestias dolore consequuntur ratione minima nihil.</p>
-    </h1>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
